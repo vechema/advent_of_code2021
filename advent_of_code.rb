4cb4e2230c4d310225ce6fb04f6b1071b93768ac
Jo Egner <14022395+vechema@users.noreply.github.com>
2022-08-14 12:54:26 -0500

    Day 09, Smoke Basin

--- advent_of_code.rb
+++ advent_of_code.rb
@@ -6,6 +6,7 @@ require './day05_hydrothermal_venture/day05'
 require './day06_lanternfish/day06'
 require './day07_the_treachery_of_whales/day07'
 require './day08_seven_segment_search/day08'
+require './day09_smoke_basin/day09'
 
 Day.subclasses.each do |day|
   day.new.display
